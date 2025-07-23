91f336ded8a7c8af1ebfaced760eee8051989366
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-07-23 09:11:51 +0200

    Initial commit

---
+++ src/main/java/co/za/tlhalesebaeng/auth_template/AuthTemplateApplication.java
@@ -0,0 +1,13 @@
+package co.za.tlhalesebaeng.auth_template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthTemplateApplication.class, args);
+	}
+
+}
